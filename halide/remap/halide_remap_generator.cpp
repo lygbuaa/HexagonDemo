56c83c9b540523cc69545fee9c2ef091e336446d
liuyongguang <liuyongguang@ainirobot.com>
2020-08-12 16:29:14 +0800

    halide remap sim ok
    input image params, w: 1920, h: 1080, c: 3
    input image stride, dim0: 1, dim1: 1920, dim2: 41984
    tableX max: 1918, tableY max: 1078
    remap cpu start
    remap cpu done, cpu_time: 0.045341
    remap hexagon done, hexagon_time: 0.018458
    diffCnt: 0
    
    Signed-off-by: liuyongguang <liuyongguang@ainirobot.com>

--- halide/remap/halide_remap_generator.cpp
+++ halide/remap/halide_remap_generator.cpp
@@ -1,6 +1,8 @@
 #include "Halide.h"
 
 namespace {
+#define SS_IMAGE_WIDTH 1920
+#define SS_IMAGE_HEIGHT 1080
 
 class HalideRemap : public Halide::Generator<HalideRemap> {
 private:
@@ -8,27 +10,33 @@ private:
 
 public:
     Input<Buffer<uint8_t>>  inputImage{"inputImage", 3};
-//    Input<Buffer<uint16_t>>  tableX{"tableX", 2};
-//    Input<Buffer<uint16_t>>  tableY{"tableY", 2};
+    Input<Buffer<uint16_t>>  tableXY{"tableXY", 3};
     Output<Buffer<uint8_t>> outputImage{"outputImage", 3};
 
     void generate() {
         // The algorithm
-//        Expr nnx = tableX(x, y);
-//        Expr nny = tableY(x, y);
-//        outputImage(x, y, c) = (inputImage(nnx, nny, c) + inputImage(nnx+1, nny, c) + inputImage(nnx, nny+1, c) + inputImage(nnx+1, nny+1, c)) << 2;
-        outputImage(x, y, c) = inputImage(x, y, c);
+        Func nnx("nnx");
+        Func nny("nny");
+        nnx(x, y) = cast<uint16_t>(clamp(tableXY(x, y, 0), 0, (SS_IMAGE_WIDTH-2)));
+        nny(x, y) = cast<uint16_t>(clamp(tableXY(x, y, 1), 0, (SS_IMAGE_HEIGHT-2)));
+//        outputImage(x, y, c) = inputImage(x, y, c); //step0: simple copy, pass
+//        outputImage(x, y, c) = inputImage(nnx(x, y), nny(x, y), c); //step1: complex copy, pass
+        outputImage(x, y, c) = (inputImage(nnx(x, y), nny(x, y), c) + inputImage(nnx(x, y)+1, nny(x, y), c) + inputImage(nnx(x, y), nny(x, y)+1, c) + inputImage(nnx(x, y)+1, nny(x, y)+1, c)) << 2;
     }
 
     void schedule() {
         Var xi{"xi"}, yi{"yi"};
-        inputImage.dim(0).set_bounds_estimate(0, 640);
-        inputImage.dim(1).set_bounds_estimate(0, 480);
+        inputImage.dim(0).set_bounds_estimate(0, SS_IMAGE_WIDTH);
+        inputImage.dim(1).set_bounds_estimate(0, SS_IMAGE_HEIGHT);
         inputImage.dim(2).set_bounds_estimate(0, 3);
 
+        tableXY.dim(0).set_bounds_estimate(0, SS_IMAGE_WIDTH);
+        tableXY.dim(1).set_bounds_estimate(0, SS_IMAGE_HEIGHT);
+        tableXY.dim(2).set_bounds_estimate(0, 2);
+
         auto output_buffer = Func(outputImage).output_buffer();
-        output_buffer.dim(0).set_bounds_estimate(0, 640);
-        output_buffer.dim(1).set_bounds_estimate(0, 480);
+        output_buffer.dim(0).set_bounds_estimate(0, SS_IMAGE_WIDTH);
+        output_buffer.dim(1).set_bounds_estimate(0, SS_IMAGE_HEIGHT);
         output_buffer.dim(2).set_bounds_estimate(0, 3);
 
         // How to schedule it
--- halide/remap/remap_main.cpp
+++ halide/remap/remap_main.cpp
@@ -40,23 +40,23 @@ Buffer<uint8_t> remap(Buffer<uint8_t> in, Buffer<uint16_t> tableX, Buffer<uint16
 
 #include "halide_remap.h"
 
-Buffer<uint8_t> test_halide_remap(Buffer<uint8_t> in, Buffer<uint16_t> tableX, Buffer<uint16_t> tableY) {
+Buffer<uint8_t> test_halide_remap(Buffer<uint8_t> in, Buffer<uint16_t> tableXY) {
     const uint16_t inW = in.width();
     const uint16_t inH = in.height();
     const uint16_t inC = in.channels();
     Buffer<uint8_t> out(inW, inH, inC);
 
     // Call it once to initialize the halide runtime stuff
-//    halide_remap(in, tableX, tableY, out);
-    halide_remap(in, out);
+    halide_remap(in, tableXY, out);
+//    halide_remap(in, out);
     // Copy-out result if it's device buffer and dirty.
     out.copy_to_host();
 
     t = benchmark(10, 1, [&]() {
         // Compute the same region of the output as blur_fast (i.e., we're
         // still being sloppy with boundary conditions)
-//        halide_remap(in, tableX, tableY, out);
-        halide_remap(in, out);
+        halide_remap(in, tableXY, out);
+//        halide_remap(in, out);
         // Sync device execution if any.
         out.device_sync();
     });
@@ -91,6 +91,7 @@ int main(int argc, char **argv) {
     Buffer<uint8_t> input(width, height, channel);
     Buffer<uint16_t> tableX(width, height);
     Buffer<uint16_t> tableY(width, height);
+    Buffer<uint16_t> tableXY(width, height, 2);
 
     const uint16_t inW = input.width();
     const uint16_t inH = input.height();
@@ -102,34 +103,54 @@ int main(int argc, char **argv) {
     const uint16_t dim2Stride = input.dim(2).stride();
     printf("input image stride, dim0: %d, dim1: %d, dim2: %d\n", dim0Stride, dim1Stride, dim2Stride);
 
+    uint16_t tableXMax = 0;
+    uint16_t tableYMax = 0;
+
     for (uint16_t y = 0; y < inH; y++) {
         for (uint16_t x = 0; x < inW; x++) {
             tableX(x, y) = (x > inW-2) ? (inW-2) : x;
             tableY(x, y) = (y > inH-2) ? (inH-2) : y;
+
+            tableXY(x, y, 0) = (x > inW-2) ? (inW-2) : x;
+            tableXY(x, y, 1) = (y > inH-2) ? (inH-2) : y;
             for(uint16_t c = 0; c < inC; c++){
                 input(x, y, c) = rand() & 0xff;
             }
+            if(tableX(x, y) > tableXMax){
+                tableXMax = tableX(x, y);
+            }
+            if(tableY(x, y) > tableYMax){
+                tableYMax = tableY(x, y);
+            }
         }
     }
+
+    printf("tableX max: %d, tableY max: %d\n", tableXMax, tableYMax);
 
     printf("remap cpu start\n");
     Buffer<uint8_t> remapCpu = remap(input, tableX, tableY);
     double cpu_time = t;
     printf("remap cpu done, cpu_time: %f\n", cpu_time);
 
-    Buffer<uint8_t> remapHalide = test_halide_remap(input, tableX, tableY);
+    Buffer<uint8_t> remapHalide = test_halide_remap(input, tableXY);
     double hexagon_time = t;
     printf("remap hexagon done, hexagon_time: %f\n", hexagon_time);
 
-//
-//    for (int y = 64; y < input.height() - 64; y++) {
-//        for (int x = 64; x < input.width() - 64; x++) {
-//            if (blurry(x, y) != speedy(x, y) || blurry(x, y) != halide(x, y)) {
-//                printf("difference at (%d,%d): %d %d %d\n", x, y, blurry(x, y), speedy(x, y), halide(x, y));
-//                abort();
-//            }
-//        }
-//    }
+    uint32_t diffCnt = 0;
+    for (uint16_t y = 0; y < inH; y++) {
+        for (uint16_t x = 0; x < inW; x++) {
+            for(uint16_t c = 0; c < inC; c++){
+//                if(y == 0){
+//                    printf("at (%d, %d, %d), remapHalide: %d\n", x, y, c, (int)remapHalide(x, y, c));
+//                }
+                if(remapHalide(x, y, c) != remapCpu(x, y, c)){
+                    ++diffCnt;
+                    printf("diff at (%d, %d, %d), remapHalide: %d, remapCpu: %d", x, y, c, (int)remapHalide(x, y, c), (int)remapCpu(x, y, c));
+                }
+            }
+        }
+    }
+    printf("diffCnt: %d\n", diffCnt);
 
     return 0;
 }
